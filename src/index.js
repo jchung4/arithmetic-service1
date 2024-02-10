58afbf9319aa0fc20e8635795d43b34664fcbafd
jchung4 <101676309+jchung4@users.noreply.github.com>
2024-02-09 19:06:35 -0500

    added operations

--- src/index.js
+++ src/index.js
@@ -8,8 +8,31 @@ app.get('/', (req, res) => {
     res.send('Arithmetic service - Hello World!');
 });
 
-app.get('/add/:n/:m', (req, res) => {
-    res.json(Number(req.params.n) + Number(req.params.m));
+app.get('/:operator/:n/:m', (req, res) => {
+    const { operator, n, m } = req.params;
+    let result;
+    switch (operator) {
+        case '+':
+            result = Number(n) + Number(m);
+            break;
+        case '-':
+            result = Number(n) - Number(m);
+            break;
+        case '*':
+            result = Number(n) * Number(m);
+            break;
+        case '/':
+            if (Number(m) === 0) {
+                result = 'Cannot divide by zero';
+            } else {
+                result = Number(n) / Number(m);
+            }
+            break;
+        default:
+            result = 'Invalid operator';
+            break;
+    }
+    res.json(result);
 });
 
 app.listen(port);
